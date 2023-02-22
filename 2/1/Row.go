5fab006adfc326c8c5e60b36352a3a60acb36c79
calgor <ahomayoni684@gmail.com>
2023-02-22 10:48:32 +0330

    moz

---
+++ 2/1/Row.go
@@ -0,0 +1,49 @@
+package main
+
+import "errors"
+
+type Row struct {
+	leftChairs, rightChair [3]Chair
+	middleChair            [4]Chair
+}
+
+func NewRow() *Row {
+	return new(Row)
+}
+func (r *Row) GetChair(c int) (bool, error) {
+	if c < 4 {
+		return r.rightChair[c-1].isFree, nil
+	} else if c > 3 && c < 8 {
+		return r.middleChair[c-4].isFree, nil
+	} else if c > 7 && c < 11 {
+		return r.rightChair[c-8].isFree, nil
+	}
+	return false, errors.New("Wrong Input")
+}
+
+func (r *Row) ToggleChair(c int) error {
+	if c < 4 {
+		r.leftChairs[c-1].SetAval(!r.leftChairs[c-1].GetAval())
+		return nil
+	} else if c > 3 && c < 8 {
+		r.leftChairs[c-4].SetAval(!r.leftChairs[c-4].GetAval())
+		return nil
+	} else if c > 7 && c < 11 {
+		r.leftChairs[c-8].SetAval(!r.leftChairs[c-8].GetAval())
+		return nil
+	}
+	return errors.New("Wrong Input")
+}
+func (r *Row) SetChair(c int, b bool) error {
+	if c < 4 {
+		r.leftChairs[c-1].SetAval(b)
+		return nil
+	} else if c > 3 && c < 8 {
+		r.leftChairs[c-4].SetAval(b)
+		return nil
+	} else if c > 7 && c < 11 {
+		r.leftChairs[c-8].SetAval(b)
+		return nil
+	}
+	return errors.New("Wrong Input")
+}
